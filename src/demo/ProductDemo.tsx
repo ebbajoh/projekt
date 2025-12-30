07984e8026463b6a8aa17b44a71077cd72fcf06d
ebbajoh <ebbajohansson999@gmail.com>
2025-12-30 13:07:07 +0100

    senaste

--- src/demo/ProductDemo.tsx
+++ src/demo/ProductDemo.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart, useShoppingCart } from "../cart";
+import { ShoppingCart, useShoppingCart } from "../SDK/cart";
 
 const products = [
   { id: "1", name: "T-shirt", price: 199 },
